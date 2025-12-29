be6ebf37d11049704b013141db868381ad9dbe85
maxime-pakula-betclic <pakulamaxime@gmail.com>
2025-12-30 00:04:18 +0100

    third commit

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+console.log("This is a popup!")
+
+const goToItewaButton = document.getElementById("goToItewaButton");
+
+goToItewaButton.addEventListener("click", () => {
+    console.log("Button was clicked!");
+});
