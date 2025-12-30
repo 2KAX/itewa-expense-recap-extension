15744a003db0353c5e8b22cc1addca79b2b6a73e
2KAX <92569019+2KAX@users.noreply.github.com>
2025-12-30 02:40:56 +0100

    ✨  User can see mail generation page (#5)

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,40 @@
+const ITEWA_EXPENSE_PAGE_URL = "https://itewa.takima.io/expenses"
 const goToItewaButton = document.getElementById("goToItewaButton");
+const mailGenerationPage = document.getElementById("mailGenerationPage");
 
-goToItewaButton.addEventListener("click", () => {
-    chrome.tabs.create({
-        url: "https://itewa.takima.io/expenses"
+document.addEventListener("DOMContentLoaded", queryTabsAndShowDynamicContent);
+
+goToItewaButton.addEventListener("click", openItewaExpenseTab);
+
+function queryTabsAndShowDynamicContent() {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        getCurrentTabAndShowDynamicContent(tabs)
     });
-});
+}
+
+function getCurrentTabAndShowDynamicContent(tabs) {
+    const currentTabUrl = tabs[0].url;
+    if(currentTabUrl === ITEWA_EXPENSE_PAGE_URL){
+        showMailGenerationPage()
+    } else {
+        showGoToItewaPage()
+    }
+}
+
+function showMailGenerationPage() {
+    console.log("You are on the expense page")
+    goToItewaButton.style.display = "none";
+    mailGenerationPage.style.display = "flex";
+}
+
+function showGoToItewaPage() {
+    console.log("You are NOT on the expense page")
+    goToItewaButton.style.display = "inline-block";
+    mailGenerationPage.style.display = "none";
+}
+
+function openItewaExpenseTab() {
+    chrome.tabs.create({
+        url: ITEWA_EXPENSE_PAGE_URL
+    });
+}
